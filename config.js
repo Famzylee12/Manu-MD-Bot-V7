89932283b5be7905412633d9f7c3c989d82a9974
Famzylee12 <fawazlekan12345@gmail.com>
2024-09-08 18:52:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "add your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&IYI1FaTZ#O3uHJc0amNWzivFSTqUn_PoFasCWKapf-rXCda7WiUo",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 MODE : process.env.MODE || "public",    
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_BIO: process.env.AUTO_BIO || "true",
+AUTO_BIO: process.env.AUTO_BIO || "false",
 READ_CMD: process.env.READ_CMD || "true",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
